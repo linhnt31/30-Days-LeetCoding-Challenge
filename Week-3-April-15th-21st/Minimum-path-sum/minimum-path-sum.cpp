ecee12e0758ebfacc9ed454d50aa456a44123ed6
viendanbac <nguyenthanhlinh58@gmail.com>
2020-04-21 08:15:10 -0700

    minimum path sum cpp

---
+++ Week-3-April-15th-21st/Minimum-path-sum/minimum-path-sum.cpp
@@ -0,0 +1,21 @@
+// Time complexity: O(m*n)... Space complexity: O(m*n)
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        // This creates a rows * cols 2D array where each element is 0.
+        // Ref: https://stackoverflow.com/questions/28663299/declaring-a-2d-vector
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        dp[0][0] = grid[0][0];
+        for(int i = 1; i < m; i++) dp[i][0] += dp[i-1][0] + grid[i][0];
+        for(int j = 1; j < n; j++) dp[0][j] += dp[0][j-1] + grid[0][j];
+        
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
