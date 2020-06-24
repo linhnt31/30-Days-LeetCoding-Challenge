dd3501cf9160946718f0ce9447834d31dd2e2516
Nguyen Thanh Linh <33242919+viendanbac@users.noreply.github.com>
2020-06-24 23:20:02 +0700

    Update space complexity

--- Generating-And-Backtracking/Letter-Case-Permutation/letter-case-permutation.cpp
+++ Generating-And-Backtracking/Letter-Case-Permutation/letter-case-permutation.cpp
@@ -1,5 +1,6 @@
 // Time complexity: O(2^n)
-// Space complexity: O(n)
+// Space complexity: O(2^n) : https://www.ideserve.co.in/learn/time-and-space-complexity-of-recursive-algorithms#:~:text=To%20conclude%2C%20space%20complexity%20of,would%20be%20O(nm).
+// The maximum depth of recurrsion tree is "n" and each function call recurrsive algorithm takes O(2^n / n) space ---> Space com:  O(n * (2^n)/n) = O(2^n)
 class Solution {
 public:
     vector<string> letterCasePermutation(string S) {
